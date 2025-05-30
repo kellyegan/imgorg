b5fb109b15a20663f87f9c7f84d5da70a77b869b
Kelly Egan <kelly.egan@gmail.com>
2025-05-30 15:03:47 -0400

    Add left right arrow movement of image

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.config import Config
+
 from kivy.uix.image import Image
 from kivy.properties import ListProperty, NumericProperty, ObjectProperty
 
@@ -10,6 +12,7 @@ from ui.widgets import ImageCard
 from catalog.db import get_all_images, add_image_list, check_catalog_duplicate  # You must have this function in catalog/db.py
 from catalog.image_importer import scan_for_images
 
+Config.set('kivy', 'exit_on_escape', '0')
 Builder.load_file("ui/imgorg.kv")
 
 class ImgOrgApp(App):
@@ -45,14 +48,17 @@ class ImgOrgApp(App):
         
         
 class ImagePreviewScreen(Screen):
-    current_image = ObjectProperty(None)
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Window.bind(on_key_down=self.on_key_down)
 
     def on_pre_enter(self):
         app = App.get_running_app()
+        self.ids["preview_box"].clear_widgets()
         app.bind(current_index=self.on_current_index_change)
         self.on_current_index_change(app, app.current_index)
         
-
+        
     def on_current_index_change(self, instance, value):
         if not isinstance(value, int):
             return
@@ -63,10 +69,25 @@ class ImagePreviewScreen(Screen):
         
         self.current_image = instance.image_list[value]
 
-        image_widget = Image(source=self.current_image["path"], allow_stretch=True, keep_ratio=True)
-        self.add_widget(image_widget)
-        
-        
+        self.ids.preview_box.clear_widgets()
+        image_widget = Image(source=self.current_image["path"], allow_stretch=True, keep_ratio=True)    
+        self.ids.preview_box.add_widget(image_widget)
+
+    def on_key_down(self, window, key, scancode, codepoint, modifier):
+        if self.manager.current != "preview":
+            return
+        app = App.get_running_app()
+
+        if key == 27:  # Escape key
+            self.manager.transition.direction = 'down'
+            self.manager.current = "browser"
+        elif key == 276:  # Left arrow key
+            new_index = (app.current_index - 1) % len(app.image_list)
+            app.set_index(new_index)
+        elif key == 275:  # Right arrow key
+            new_index = (app.current_index + 1) % len(app.image_list)
+            app.set_index(new_index)
+
 class ThumbnailBrowserScreen(Screen):
     columns = 1
     thumbnail_width = 200
